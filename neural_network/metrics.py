1d2ca22670c8b5186ef65c7cf3191fdd15c5a385
Q220036 <j.fransolet@student.helmo.be>
2026-03-25 14:18:10 +0100

    FIX : Increase efficiency

--- neural_network/metrics.py
+++ neural_network/metrics.py
@@ -1,6 +1,5 @@
 ﻿# metrics.py
 
-from .formula import matrix_confusion
 from enum import Enum
 
 class MetricType(Enum):
@@ -23,58 +22,51 @@ def calculate_mse(y_true: list[float], y_pred: list[float])-> float:
         raise ArithmeticError("Impossible de diviser par 0.")
     return sum((yt - yp) ** 2 for yt, yp in zip(y_true, y_pred))
 
-def calculate_accuracy(y_true: list[float], y_pred_probs: list[float], threshold=0.5)-> float:
+def calculate_accuracy(tp: int, tn: int, fp: int, fn: int)-> float:
     """
     Calculer le taux de fiabilité du modèle.
 
-    :param y_true: Les valeurs réelles.
-    :param y_pred_probs: La probabilité que la valeur appartient à la classe.
-    :param threshold: La valeur limite pour être dans une catégorie.
+    :param tp: True Positive
+    :param tn: True Negative
+    :param fp: False Positive
+    :param fn: False Negative
     :return: Le taux de fiabilité.
     """
-    tp, tn, fp, fn = matrix_confusion(y_true, y_pred_probs, threshold)
     total = tp + tn + fp + fn
-
     return (tp + tn) / total if total > 0 else 0.0
 
-def calculate_precision(y_true: list[float], y_pred_probs: list[float], threshold=0.5)-> float:
+def calculate_precision(tp: int, fp: int)-> float:
     """
     Calculer le taux de précision du modèle.
 
-    :param y_true: Les valeurs réelles.
-    :param y_pred_probs: La probabilité que la valeur appartient à la classe.
-    :param threshold: La valeur limite pour être dans une catégorie.
+    :param tp: True Positive
+    :param fp: False Positive
     :return: Le taux de précision.
     """
-    tp, tn, fp, fn = matrix_confusion(y_true, y_pred_probs, threshold)
-
     return tp / (tp + fp) if (tp + fp) > 0 else 0.0
 
-def calculate_recall(y_true: list[float], y_pred_probs: list[float], threshold=0.5)-> float:
+def calculate_recall(tp: int, fn: int)-> float:
     """
     Calculer la valeur "recall" est le taux de vrai positif sur la quantité réelle de positif.
 
-    :param y_true: Les valeurs réelles.
-    :param y_pred_probs: La probabilité que la valeur appartient à la classe.
-    :param threshold: La valeur limite pour être dans une catégorie.
+    :param tp: True Positive
+    :param fn: False Negative
     :return: Le taux de précision.
     """
-    tp, tn, fp, fn = matrix_confusion(y_true, y_pred_probs, threshold)
-
     return tp / (tp + fn) if (tp + fn) > 0 else 0.0
 
-def calculate_f1_score(y_true: list[float], y_pred_probs: list[float], threshold=0.5)-> float:
+def calculate_f1_score(tp: int, fp: int, fn: int)-> float:
     """
     Calculer la moyenne harmonique entre la précision et le rappel.
 
-    :param y_true: Les valeurs réelles.
-    :param y_pred_probs: La probabilité que la valeur appartient à la classe.
-    :param threshold: La valeur limite pour être dans une catégorie.
+    :param tp: True Positive
+    :param fp: False Positive
+    :param fn: False Negative
     :return: Le taux de précision.
     """
 
-    precision = calculate_precision(y_true,y_pred_probs, threshold)
-    recall = calculate_recall(y_true,y_pred_probs, threshold)
+    precision = calculate_precision(tp, fp)
+    recall = calculate_recall(tp, fn)
 
     if (precision + recall) > 0:
         value = 2 * (precision * recall) / (precision + recall)
